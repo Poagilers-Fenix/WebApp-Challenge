580ecdb1a52381e894c5fc75220fca618617129e
Eric-Pessoa <pessoa.eric@gmail.com>
2021-10-02 13:39:40 -0300

    chore: refactoring of css, change in controller

---
+++ Cardapp.WebApp/Cardapp.WebApp/Controllers/EstabelecimentoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cardapp.WebApp.Controllers
+{
+    public class EstabelecimentoController : Controller
+    {
+
+        public IActionResult CadastroGerente()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroEstabelecimento()
+        {
+            return View();
+        }
+
+    }
+}
